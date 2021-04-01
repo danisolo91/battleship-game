d008813474f4af5ca99643ef8c7ecfc444835849
Daniel <danisolo91@gmail.com>
2021-04-01 22:20:08 +0200

    Created App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import GameBoards from "./components/GameBoards";
+import Nickname from "./components/Nickname"
+import { GameContext } from "./contexts/GameContext";
+
 const App = () => {
+
+    const game = useContext(GameContext)[0];
+
     return (
         <>
-            Battleship
+            <h1>Battleship</h1>
+            <div className="container">
+                {game.players.length === 0 ?
+                    <Nickname /> :
+                    <GameBoards />
+                }
+            </div>
         </>
     );
 };
